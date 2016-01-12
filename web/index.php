20357b0659e6d24c6561d54ec9b2c53b45b89d6d
Ahmet Kiyak <s.ahmet.kiyak@gmail.com>
2016-01-12 01:11:42 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,7 +5,8 @@ require('../vendor/autoload.php');
 $app = new \Slim\Slim();
 
 $app->get('/', function () {
-    echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname() . "\n";
+    echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname()
+    . " and ip: " . $_SERVER['SERVER_ADDR'] . "\n";
 });
 
 $app->run();
